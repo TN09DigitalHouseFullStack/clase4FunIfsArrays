596c213feeb4a405f107b2159661a6ddc252fd42
daanduqueutp <daanduque@utp.edu.co>
2022-02-24 17:27:06 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//variables var, let y const
+
+let telefonoMadre = 123456678;
+const telefono_madre = 123456678;
+
+//bloques
+
+// bloque 1
+if(true){
+    let nombre = 'Daniel';
+    console.log(nombre);
+}
+// bloque 2
+if(true){
+    let nombre = 'Andrés';
+    console.log(nombre);
+}
+
+// bloque error
+// if(true){
+//     let nombre = 'Daniel';
+//     console.log(nombre);
+//     let nombre = 'Andrés';
+
+//     console.log(nombre);
+// }
+
+// console.log(nombre);
+
+//const -> no es mutable
+
+const edad = 50;
+
+edad = 25;
+
